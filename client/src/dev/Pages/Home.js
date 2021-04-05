ec575cf8cba21588fa93a3db08027a1e80d09946
Basile Leroy <basile.leroy.jnm@hotmail.com>
2021-04-05 21:15:54 +0200

    Changing name handler in home.js

--- client/src/dev/Pages/Home.js
+++ client/src/dev/Pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     const [user, setUser] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleLogIn = (event) => {
+    const handleRegister = (event) => {
         event.preventDefault();
 
         if ( user !== "" && password !== "") {
@@ -27,7 +27,7 @@ const Home = () => {
             <div className="flex flex-row h-auto z-0">
 
                     <div className="flex flex-col w-nine m-5 text-2xl">
-                    <form action="/chats" method="post" onSubmit={handleLogIn}>
+                    <form action="/chats" method="post" onSubmit={handleRegister}>
                         <h1 className="text-4xl text-center mt-8 mb-16">Register as user</h1>
                     
                         <label className="mb-1">User</label>
@@ -37,7 +37,7 @@ const Home = () => {
                         <input type="password" placeholder="Your password ..." className="pl-2 py-2 rounded-lg" onChange={(e) => {setPassword(e.target.value)}} required/>
                         <br/>
                         <div className="flex flex-row my-6 justify-around">
-                            <button type="submit" className="text-brown-white hover:underline bg-brown-yellow rounded-br-lg rounded-tl-lg p-3 2xl:text-lg 2xl:p-4 mb-3" onClick={handleLogIn}>Register</button>
+                            <button type="submit" className="text-brown-white hover:underline bg-brown-yellow rounded-br-lg rounded-tl-lg p-3 2xl:text-lg 2xl:p-4 mb-3" onClick={handleRegister}>Register</button>
                         </div>
                     </form>
                     </div>
